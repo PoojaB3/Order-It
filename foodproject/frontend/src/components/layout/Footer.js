1785215b45c49af739d0f4f18304b1205d93e775
Poojab3 <92905612+Poojab3@users.noreply.github.com>
2023-09-29 16:16:13 +0530

    OrderIt

---
+++ foodproject/frontend/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='py-1'>
+            <p className='text-center mt-2'>Food Delivery Website 2023-24, All Rights Reserved</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
